fc72b40023c95632eb3e2fbc1322d39bdf87837b
jason <pnxl@pm.me>
2022-12-15 09:28:53 +0000

    starting bot code

---
+++ main.js
@@ -0,0 +1,24 @@
+import { create } from "venom-bot";
+
+create({
+  session: "class-hamster", //name of session
+})
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body === "Hi") {
+      client
+        .sendText(message.from, "Welcome Venom 🕷")
+        .then((result) => {
+          console.log("Result: ", result); //return object success
+        })
+        .catch((erro) => {
+          console.error("Error when sending: ", erro); //return object error
+        });
+    }
+  });
+}
